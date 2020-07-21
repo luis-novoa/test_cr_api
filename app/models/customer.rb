802cf9b90a4920b921087b98415779e29a673149
Luis Novoa <luis.matte.novoa@gmail.com>
2020-07-21 17:43:36 -0400

    Codes customer.name to capitalize first letter

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,12 @@
 class Customer < ApplicationRecord
+  before_save :capitalize_first_letter
+
   has_many :carts
+
+  private
+
+  def capitalize_first_letter
+    name.capitalize!
+  end
+  
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -4,4 +4,13 @@ RSpec.describe Customer, type: :model do
   context 'relationships' do
     it { is_expected.to have_many :carts }
   end
+
+  context "name" do
+    let(:test) { Customer.new(name: 'test') }
+    it "first letter is always capitalized" do
+      test.save
+      expect(test.name).to eq('Test') 
+    end
+  end
+  
 end
