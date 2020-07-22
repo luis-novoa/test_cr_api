c7367766efce299804fa82584f9498d8f13379ad
Luis Novoa <luis.matte.novoa@gmail.com>
2020-07-22 18:38:46 -0400

    Adds validations to the cart_item model

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,7 @@
 class CartItem < ApplicationRecord
+  validates :cart_id, presence: true, numericality: { greater_than: 0 }
+  validates :medicine_id, presence: true, numericality: { greater_than: 0 }
+
   belongs_to :cart
   belongs_to :medicine
 end
--- spec/models/cart_item_spec.rb
+++ spec/models/cart_item_spec.rb
@@ -5,4 +5,11 @@ RSpec.describe CartItem, type: :model do
     it { is_expected.to belong_to :cart }
     it { is_expected.to belong_to :medicine }
   end
+
+  context 'validations' do
+    it { is_expected.to validate_presence_of(:cart_id) }
+    it { is_expected.to validate_numericality_of(:cart_id).is_greater_than(0) }
+    it { is_expected.to validate_presence_of(:medicine_id) }
+    it { is_expected.to validate_numericality_of(:medicine_id).is_greater_than(0) }
+  end
 end
