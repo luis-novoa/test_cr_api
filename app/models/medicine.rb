374d4572e550dcaff2a9a41386635edd83325cb3
Luis Novoa <luis.matte.novoa@gmail.com>
2020-07-22 13:58:16 -0400

    Adds validations for the medicine model

--- app/models/medicine.rb
+++ app/models/medicine.rb
@@ -1,4 +1,8 @@
 class Medicine < ApplicationRecord
+  validates :name, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 2, maximum: 50 }
+  validates :value, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :stock, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
   def total
     value * quantity
   end
